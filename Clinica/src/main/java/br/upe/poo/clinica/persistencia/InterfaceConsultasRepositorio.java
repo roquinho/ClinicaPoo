c4034ba627b0f2c7fdf8e863a9b60a612b97fad5
manoel <manoel@roquinho>
2016-06-13 23:51:30 -0300

    ultima versao

--- Clinica/src/main/java/br/upe/poo/clinica/persistencia/InterfaceConsultasRepositorio.java
+++ Clinica/src/main/java/br/upe/poo/clinica/persistencia/InterfaceConsultasRepositorio.java
@@ -2,10 +2,16 @@
 package br.upe.poo.clinica.persistencia;
 
 import br.upe.poo.clinica.entidades.Consultas;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface InterfaceConsultasRepositorio extends CrudRepository<Consultas, Long> {
     
+    public List<Consultas> findByDiaConsulta(Date diaConsulta);
     
+    public Consultas findByCpfPaciente(Long cpf);
+    
+    public Consultas findByCpfMedicos(Long cpf);
 }
