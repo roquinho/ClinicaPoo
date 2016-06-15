c5356175707cac7ca13b8d0b41c2d90d3175a79c
manoel <manoel@roquinho>
2016-06-14 23:40:08 -0300

    conserto

--- Clinica/src/main/java/br/upe/poo/clinica/persistencia/InterfaceConsultasRepositorio.java
+++ Clinica/src/main/java/br/upe/poo/clinica/persistencia/InterfaceConsultasRepositorio.java
@@ -2,16 +2,10 @@
 package br.upe.poo.clinica.persistencia;
 
 import br.upe.poo.clinica.entidades.Consultas;
-import java.util.Date;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface InterfaceConsultasRepositorio extends CrudRepository<Consultas, Long> {
     
-    public List<Consultas> findByDiaConsulta(Date diaConsulta);
     
-    public Consultas findByCpfPaciente(Long cpf);
-    
-    public Consultas findByCpfMedicos(Long cpf);
 }
--- Clinica/src/main/java/br/upe/poo/clinica/persistencia/InterfaceExamesRepositorio.java
+++ Clinica/src/main/java/br/upe/poo/clinica/persistencia/InterfaceExamesRepositorio.java
@@ -2,14 +2,9 @@
 package br.upe.poo.clinica.persistencia;
 
 import br.upe.poo.clinica.entidades.Exames;
-import java.util.Date;
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 
 public interface InterfaceExamesRepositorio extends CrudRepository<Exames, Long>{
     
-    public List<Exames> findByDiaExames(Date diaExame);
-    
-    public Exames findByCpfPaciente(Long cpf);
 }
